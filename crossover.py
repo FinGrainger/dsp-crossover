09c75943563812765d69186ecf97377e7b013f8f
FinGrainger <fin.grainger1@gmail.com>
2026-06-09 01:17:15 +0100

    Added saving of graphs

--- crossover.py
+++ crossover.py
@@ -22,17 +22,17 @@ def main():
         print('2 - Frossover Filter Response')
         choice = input()
         lp, hp = filter_choice(fc, fs)
-        if choice is '1':
+        if choice == '1':
             plot_graph(fc, fs, lp, hp)
-        elif choice is '2':
+        elif choice == '2':
             plot_response(lp, hp, fc, fs)
     
         print("Plot Another (y/n)")
-        if input() is 'y':
+        if input() == 'y':
             new_graph = True
         else:
             new_graph = False
-    
+
     #plot_graph()
 
 # - Filter - x order butterworth
@@ -59,22 +59,22 @@ def filter_choice(fc, fs):
     filter_choice = input()
 
     print("Choose filter order: ")
-    if filter_choice is '1':
+    if filter_choice == '1':
         print ("1, 2, 3, 4 ,5, 6, 7, 8")
         order_choice = int(input())
         lp, hp = butterworth_crossover(fc, order_choice, fs) 
-    elif filter_choice is '2':
+    elif filter_choice == '2':
         print("2, 4, 6, 8")
         order_choice = int(input())
         lp, hp = lr_crossover(fc, order_choice, fs)
 
     print("lp, hp, both or none(1,2,3,4)")
     choice_filter = int(input())
-    if choice_filter is 1:
+    if choice_filter == 1:
         return lp, None
-    elif choice_filter is 2:
+    elif choice_filter == 2:
         return None, hp
-    elif choice_filter is 3:
+    elif choice_filter == 3:
         return lp, hp
     else:
         return None, None
@@ -101,17 +101,16 @@ def plot_response(lp_sos, hp_sos, fc, fs):
     plt.axvline(x=fc, color='grey', linestyle='--', label=f'Crossover: {fc}Hz')
     plt.axhline(y=-6, color='red', linestyle=':', label='-6dB reference')
     plt.legend()
-
     combined = 20 * np.log10(np.abs(np.abs(h_lp) + np.abs(h_hp)))
     plt.plot(w, combined, label='Combined', linestyle='--', color='green')
-
+    save_graph(plt.gcf())
     plt.show()
 
 # - Audio file filter processing
 filename = 'jungler.wav' #Temp file
 def audio_process(filename):
     audio, file_fs = sf.read('jungler.wav') # Load audio file
-    if audio.ndim is 2:
+    if audio.ndim == 2:
         audio = audio[:, 0] # Turn mono (duplicate left ear)    
     return audio, file_fs
 audio, file_fs = audio_process(filename)
@@ -133,8 +132,8 @@ def plot_spectrum(audio, file_fs, fc, lp_sos=None, hp_sos=None, title='audio spe
     
     print('Do you want to use Welch method? (y/n)')
     yn = input()
-    if yn is 'y':
-        
+    if yn == 'y':
+
         spectrum_freqs, spectrum_power = welch(audio, fs=file_fs, nperseg=4096) #Welch method returns a smoother curve, overlapping segments of audio
         spectrum_mag = 10 * np.log10(spectrum_power)    
         raw_max = np.max(spectrum_mag)  # save the max before normalising
@@ -146,14 +145,14 @@ def plot_spectrum(audio, file_fs, fc, lp_sos=None, hp_sos=None, title='audio spe
         spectrum_mag = spectrum_mag - np.max(spectrum_mag)
     
 
-    if lp_sos is not None: #Checks if none
+    if lp_sos is None: #Checks if none
         w, h_lp = sosfreqz(lp_sos, worN=1000, fs=file_fs) 
         lp_db = 20 * np.log10(abs(h_lp)) #Convert to dB
         plt.plot(w, lp_db, label='Low Pass Filter')#Plotting filters
 
         Plot_filter_welch(lp_sos, raw_max, 'yellow')
 
-    if hp_sos is not None:
+    if hp_sos is None:
         w, h_hp = sosfreqz(hp_sos, worN=1000, fs=file_fs)
         hp_db = 20 * np.log10(abs(h_hp))
         plt.plot(w, hp_db, label='High Pass Filter')
@@ -174,10 +173,12 @@ def plot_spectrum(audio, file_fs, fc, lp_sos=None, hp_sos=None, title='audio spe
     ax = plt.gca()
     ax.set_xticks([20, 30, 50, 100, 200, 500, 1000, 2000, 5000, 10000, 20000])
     ax.get_xaxis().set_major_formatter(ticker.ScalarFormatter())
-
+        
+    save_graph(plt.gcf())
+        
     print('Do you want to save audio (y/n)')
     yn = input();
-    if yn is 'y':
+    if yn == 'y':
         
         if lp_sos is not None:
             audio_saving(lp_sos, audio, 'Low Pass Sample', file_fs)
@@ -197,5 +198,15 @@ def plot_graph(fc, fs, lp, hp):
 def audio_saving(filter, audio, filter_type, file_fs):
     sample = sosfilt(filter, audio)
     sf.write(f'{filter_type}.wav', sample, file_fs)
+
+def save_graph(f):
+    print ('Do you want to save graph (y/n)')
+    yn = input()
+    if yn == ('y'):
+        print("Name: ")
+        name = input()
+        f.savefig(f'{name}.png')
+   
+
     
 main()
